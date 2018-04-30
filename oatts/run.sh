ada1d0afded437141ea590d08d453e9140f86df4
Samantha Cohen <samanthacohen294@gmail.com>
2018-04-30 15:30:39 +0100

    replace with swagger 3.0

--- oatts/run.sh
+++ oatts/run.sh
@@ -1,7 +1,5 @@
-npm install -g oatts
-npm install -g mocha
-npm install --save mocha
-npm install --save chakram
+rm -rf generated
+mkdir generated
 oatts generate -s swagger.yaml -w ./generated
 ls -hal generated
 mocha --recursive generated/
---
+++ oatts/setup-and-run.sh
@@ -0,0 +1,5 @@
+npm install -g oatts
+npm install -g mocha
+npm install --save mocha
+npm install --save chakram
+./run.sh
